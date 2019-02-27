b257e1fe3c86fefb4567422d6adfa5e75ef54ef1
Hari om ojha <47915211+omhariojha@users.noreply.github.com>
2019-02-27 13:43:50 +0530

    Added new project

---
+++ codeigniter-crud/application/views/templates/footer.php
@@ -0,0 +1,3 @@
+<p><em>Copyright ©Technarayana software pvt ltd 2019</em></p>
+</body>
+</html>
---
+++ codeigniter-crud/application/views/templates/header.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+		<title>CodeIgniter CRUD</title>
+	</head>
+	<body>
+		<h1>Simple CRUD</h1>
+		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
